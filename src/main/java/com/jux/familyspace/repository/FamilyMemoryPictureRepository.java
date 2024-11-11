caa92cdd2f5842b5475cddc91a805d9a9694ce55
jux42 <roujux@gmail.com>
2024-11-11 01:44:58 +0100

    date corrections

--- src/main/java/com/jux/familyspace/repository/FamilyMemoryPictureRepository.java
+++ src/main/java/com/jux/familyspace/repository/FamilyMemoryPictureRepository.java
@@ -2,8 +2,12 @@ package com.jux.familyspace.repository;
 
 import com.jux.familyspace.model.FamilyMemoryPicture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 
 @Repository
 public interface FamilyMemoryPictureRepository extends JpaRepository<FamilyMemoryPicture, Long> {
+    Iterable<FamilyMemoryPicture> getFamilyMemoryPicturesByDate(@DateTimeFormat(pattern = "yyyy-MM-dd")Date date);
 }
--- src/main/java/com/jux/familyspace/repository/HaikuRepository.java
+++ src/main/java/com/jux/familyspace/repository/HaikuRepository.java
@@ -2,8 +2,12 @@ package com.jux.familyspace.repository;
 
 import com.jux.familyspace.model.Haiku;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 
 @Repository
 public interface HaikuRepository extends JpaRepository<Haiku, Long> {
+    Iterable<Haiku> getHaikusByDate(@DateTimeFormat(pattern = "yyyy-MM-dd")Date date);
 }
