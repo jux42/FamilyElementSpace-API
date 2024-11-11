8d5aa670b5530dcc0580225a61e4a0ec217a5cde
jux42 <roujux@gmail.com>
2024-11-11 19:11:26 +0100

    Guest Login

--- src/main/java/com/jux/familyspace/configuration/SecurityConfig.java
+++ src/main/java/com/jux/familyspace/configuration/SecurityConfig.java
@@ -1,32 +1,35 @@
 package com.jux.familyspace.configuration;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/auth/*").permitAll();
+                    auth.requestMatchers("/user/*").authenticated();
                     auth.anyRequest().permitAll();
-                });
 
+
+                });
 
         return http.build();
     }
--- src/main/java/com/jux/familyspace/controller/AuthController.java
+++ src/main/java/com/jux/familyspace/controller/AuthController.java
@@ -1,14 +1,14 @@
 package com.jux.familyspace.controller;
 
 import com.jux.familyspace.service.AuthService;
+import com.jux.familyspace.service.GuestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,6 +16,7 @@ import java.io.IOException;
 public class AuthController {
 
     private final AuthService authService;
+    private final GuestService guestService;
 
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password) {
@@ -31,4 +32,16 @@ public class AuthController {
     public void memberRegister(@RequestParam String username, @RequestParam String password) throws IOException {
         authService.register(username, password, true);
     }
+
+    @PostMapping("/guestlogin")
+    public ResponseEntity<String> loginAsGuest(@RequestParam String username) {
+        return ResponseEntity.ok(guestService.loginAsGuest(username));
+    }
+
+    @GetMapping("/guestname")
+    public ResponseEntity<String> getGuestName(@RequestHeader("Authorization") String authorizationHeader) {
+        return ResponseEntity.ok(guestService.getGuestName(authorizationHeader));
+    }
+
+
 }
