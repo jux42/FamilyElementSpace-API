133f52d9dd538a464383b9aa562de0920ee1a193
jux42 <roujux@gmail.com>
2024-11-17 23:00:03 +0100

    refacto

--- src/main/java/com/jux/familyspace/component/HaikuSizeTracker.java
+++ src/main/java/com/jux/familyspace/component/HaikuSizeTracker.java
@@ -9,25 +9,25 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Component
 public class HaikuSizeTracker implements ElementSizeTrackerInterface<Haiku> {
 
-    private final AtomicInteger totalSize = new AtomicInteger();
+    private final AtomicInteger totalHaikusSize = new AtomicInteger();
 
     @Override
     public int getTotalSize() {
-        return totalSize.get();
+        return totalHaikusSize.get();
     }
 
     @Override
     public void setTotalSize(int size) {
-        totalSize.set(size);
+        totalHaikusSize.set(size);
     }
 
     @Override
     public void incrementSize(int delta) {
-        totalSize.addAndGet(delta);
+        totalHaikusSize.addAndGet(delta);
     }
 
     @Override
     public void decrementSize(int delta) {
-        totalSize.addAndGet(-delta);
+        totalHaikusSize.addAndGet(-delta);
     }
 }
